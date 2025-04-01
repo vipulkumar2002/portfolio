d9d5f35c61bd54fc83375b69a0fc31385276a1e0
vipulkumar2002 <vipulkumarpatel@2002gmail.com>
2025-04-02 00:22:17 +0530

    fix social media links

--- src/components/common/socialMedia/SocialMedia.tsx
+++ src/components/common/socialMedia/SocialMedia.tsx
@@ -2,32 +2,32 @@ import { FaGithub, FaLinkedinIn } from "react-icons/fa"
 
 const SocialMedia = () => {
   return (
-      <div className="flex gap-4">
-              <a
-                href='https://github.com/'
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="Open github profile"
-                data-toggle="tooltip"
-                title="Github"
-              >
-                <span className="bannerIcon">
-                  <FaGithub />
-                </span>
-              </a>
-              <a
-                href='https://www.linkedin.com/'
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="Open linkedin profile"
-                data-toggle="tooltip"
-                title="Linkedin"
-              >
-                <span className="bannerIcon">
-                  <FaLinkedinIn />
-                </span>
-              </a>
-            </div>
+    <div className="flex gap-4">
+      <a
+        href='https://github.com/vipulkumar2002'
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Open github profile"
+        data-toggle="tooltip"
+        title="Github"
+      >
+        <span className="bannerIcon">
+          <FaGithub />
+        </span>
+      </a>
+      <a
+        href='https://www.linkedin.com/in/vipul-p-46171118a/'
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Open linkedin profile"
+        data-toggle="tooltip"
+        title="Linkedin"
+      >
+        <span className="bannerIcon">
+          <FaLinkedinIn />
+        </span>
+      </a>
+    </div>
   )
 }
 
